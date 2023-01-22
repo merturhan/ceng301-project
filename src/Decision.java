f1cd278c07b9009111e368fa1f0e36dfac3bce93
Mert Urhan <mert-urhan@hotmail.com>
2023-01-22 16:56:11 +0300

    Decision Created

---
+++ src/Decision.java
@@ -0,0 +1,7 @@
+public class Decision {
+    private int DecisionID;
+    private int ApartmentID;
+    private String DecisionDescription;
+    private int VoteYes;
+    private int DecisionDate;
+}
