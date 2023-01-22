5a02a6e33c2523cd5bdfa6e54035728382caac64
Mert Urhan <mert-urhan@hotmail.com>
2023-01-22 15:28:20 +0300

    Update ApartmentView.java

--- src/ApartmentView.java
+++ src/ApartmentView.java
@@ -45,6 +45,52 @@ public class ApartmentView implements  ViewInterface{
         return new ViewData("MainMenu", "");
     }
 
+    ViewData insertOperation(ModelData modelData) throws Exception {
+        System.out.println("Number of inserted rows is " + modelData.recordCount);
+
+        return new ViewData("MainMenu", "");
+    }
+
+    ViewData updateOperation(ModelData modelData) throws Exception {
+        System.out.println("Number of updated rows is " + modelData.recordCount);
+
+        return new ViewData("MainMenu", "");
+    }
+
+    ViewData deleteOperation(ModelData modelData) throws Exception {
+        System.out.println("Number of deleted rows is " + modelData.recordCount);
+
+        return new ViewData("MainMenu", "");
+    }
+
+    Map<String, Object> getWhereParameters() throws Exception {
+        System.out.println("Filter conditions:");
+        Integer apartmentID = getInteger("Apartment ID : ", true);
+        String apartmentName = getString("Apartment Name : ", true);
+        String street = getString("Street : ", true);
+        String city = getString("City : ", true);
+        String state = getString("State : ", true);
+        Integer zip_code = getInteger("Zip Code : ", true);
+
+
+        Map<String, Object> whereParameters = new HashMap<>();
+        if (apartmentID != null) whereParameters.put("apartmentID", apartmentID);
+        if (apartmentName != null) whereParameters.put("apartmentName", apartmentName);
+        if (street != null) whereParameters.put("street", street);
+        if (city != null) whereParameters.put("city", city);
+        if (state != null) whereParameters.put("state", state);
+        if (zip_code != null) whereParameters.put("zip_code", zip_code);
+
+        return whereParameters;
+    }
+
+    ViewData selectGUI(ModelData modelData) throws Exception {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("whereParameters", getWhereParameters());
+
+        return new ViewData("Apartment", "select", parameters);
+    }
+
     
 
 }
